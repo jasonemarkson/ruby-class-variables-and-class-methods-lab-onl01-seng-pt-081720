bd7d31e4ef709b3c1f54a6390deae13d669cac42
Jason Markson <jasonmarkson76@gmail.com>
2020-09-06 20:01:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Song 
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre 
+
+
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @@artists << artist
+    @@genres << genre
+    @name = name
+    @artist = artist 
+    @genre = genre
+
+  end
+
+
+  def self.count
+    @@count
+  end
+  
+  def self.artists 
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def genre_count
+    @@genres.collect do |gen|
+      gen => gen.count
+    end
+  end
+end
